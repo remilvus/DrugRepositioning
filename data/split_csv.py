de01f9ce831d7a5b4befd6b7c12160297f59dd2e
mateuszpach <mateusz.pach.05@gmail.com>
2023-05-04 19:13:18 +0200

    Add organised data

---
+++ data/split_csv.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Load the CSV file into a Pandas dataframe
+df = pd.read_csv('docked_repurposing.csv')
+
+# Group the dataframe by the target column
+groups = df.groupby('target')
+
+# Loop through each group
+for name, group in groups:
+    # Extract the relevant columns
+    cols = ['smiles', 'ChEMBL_ID', 'activity', 'target', name]
+    sub_df = group[cols]
+
+    # Rename the last column to 'binding score'
+    sub_df = sub_df.rename(columns={name: 'binding_score'})
+
+    # Save the sub-dataframe to a CSV file
+    filename = name + '.csv'
+    sub_df.to_csv(filename, index=False)
