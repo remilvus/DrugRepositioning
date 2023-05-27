a3954a7172761f1c7b02ec95ace482aa850ddec9
remilvus <remilvus@gmail.com>
2023-05-27 14:36:38 +0200

    Add shuffle

--- src/datasets/ligand_target_activity_and_binding.py
+++ src/datasets/ligand_target_activity_and_binding.py
@@ -1,3 +1,5 @@
+import random
+
 import torch
 from copy import deepcopy
 from dataclasses import dataclass
@@ -42,6 +44,7 @@ class LigandTargetActivityAndBinding(Dataset):
         targets: set,
         ligand_featurizer: Featurizer,
         target_featurizer: Featurizer,
+        shuffle=True,
     ):
         self.data = []
         self.ligands = {}
@@ -65,6 +68,8 @@ class LigandTargetActivityAndBinding(Dataset):
                         DataPoint(ligand, None, target, None,
                                   activity_Ki=activity_Ki,activity_IC50=activity_IC50,binding_score=binding_score)
                     )
+        if shuffle:
+            random.shuffle(self.data)
 
     def __getitem__(self, index):
         data_point = deepcopy(
